56ab946c23438d825edc100f8d7860f3b0d01c27
Nathan Dissoubray <nathan.dissoubray@rte-france.com>
2026-01-15 15:16:53 +0100

    Remove old unneeded comment
    Signed-off-by: Nathan Dissoubray <nathan.dissoubray@rte-france.com>

--- network-area-diagram/src/main/java/com/powsybl/nad/layout/Atlas2ForceLayout.java
+++ network-area-diagram/src/main/java/com/powsybl/nad/layout/Atlas2ForceLayout.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * Copyright (c) 2025-2026, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -36,7 +36,6 @@ public class Atlas2ForceLayout extends AbstractLayout {
     private static final double SCALE_EXPONENT = -0.458;
 
     private final Setup<Node, Edge> setup;
-    //maybe change the class name to not be confused with NAD LayoutParameters ?
     private final Atlas2Parameters atlas2Parameters;
     private final PostProcessing<Node, Edge> postProcessing;
 
